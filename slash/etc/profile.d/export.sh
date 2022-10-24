24fbc65ee074a915595de8f99ef8a9c11f6f1b4a
PicsuBoy <73409029+PicsuBoy@users.noreply.github.com>
2022-10-24 14:37:20 +0100

    Create export.sh

---
+++ slash/etc/profile.d/export.sh
@@ -0,0 +1,5 @@
+#<XDG>
+export XDG_DATA_DIRS=$HOME/.nix-profile/share:/usr/local/share:/usr/share:/var/lib/snapd/desktop
+#<JAVA>
+export _JAVA_AWT_WM_NONREPARENTING=1
+export JAVA_HOME=/usr/lib/jvm/jdk-19
