436533a897cf8cd2a767ca0d6ce1e9accfb33a9c
PicsuBoy <73409029+PicsuBoy@users.noreply.github.com>
2022-11-05 20:10:00 +0100

    Add files via upload

--- slash/etc/profile.d/export.sh
+++ slash/etc/profile.d/export.sh
@@ -1,5 +1,21 @@
 #<XDG>
 export XDG_DATA_DIRS=$HOME/.nix-profile/share:/usr/local/share:/usr/share:/var/lib/snapd/desktop
+export XDG_DATA_HOME=$HOME/.local/share
+
 #<JAVA>
 export _JAVA_AWT_WM_NONREPARENTING=1
 export JAVA_HOME=/usr/lib/jvm/jdk-19
+export PATH=$PATH:$JAVA_HOME/bin
+
+#<COLORS>
+export COLORS_C1=#FFF289
+
+export COLORS_B1=#000500
+export COLORS_B1_L1=#111111
+export COLORS_B1_L2=#222222
+export COLORS_B1_T1=#000500F2
+
+export COLORS_F1=#EEEEEE
+export COLORS_F1_D1=#969696
+export COLORS_F1_D2=#3D3D3D
+export COLORS_F1_D5=#222222
